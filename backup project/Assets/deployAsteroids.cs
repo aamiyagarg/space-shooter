2c58b72d7a1372b09729bdea9b917ab0be6c1fca
aamiyagarg <52558992+aamiyagarg@users.noreply.github.com>
2019-10-04 21:18:24 +0530

    as

---
+++ backup project/Assets/deployAsteroids.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployAsteroids : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+
+    }
+    IEnumerator asteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}
